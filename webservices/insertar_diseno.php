7911abe7742375b930be4cd6f08628a89c142004
lchaconc <luis.chacon.campos@gmail.com>
2020-06-24 14:42:48 -0600

    Fin imp back-end de agregar diseño
    -Se crean las tablas en la BD
    -Se crean las realciones
    -Se nsertan registros

--- webservices/insertar_diseno.php
+++ webservices/insertar_diseno.php
@@ -1,9 +1,9 @@
 <?php
 	require_once('conectar.php');
 	$conexion=conectarDB();		
-	$datos= array("idSolicitante"=>10, "idProductoDiseno"=>6, "descripcion"=>"Prueba paty oscar" );	
+	$datos= array("idUsuario"=>1, "idProductoDiseno"=>6, "descripcion"=>"Prueba 202020" );	
 	$sentencia= $conexion->prepare("CALL INSERT_DISENO (?,?,?)");
-	$sentencia->bind_param("iis", $datos["idSolicitante"], $datos["idProductoDiseno"], $datos["descripcion"] );
+	$sentencia->bind_param("iis", $datos["idUsuario"], $datos["idProductoDiseno"], $datos["descripcion"] );
 	
 	$sentencia->execute();
 	$sentencia->close();
