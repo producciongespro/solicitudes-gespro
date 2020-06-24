06848ccaf844c27c933eef65cab7a1ee8b007a01
lchaconc <luis.chacon.campos@gmail.com>
2020-06-24 15:13:59 -0600

    Update insertar_diseno.php
    -Se crea el API insertar solcicutd diseño
    -Se realizan las pruebas en Postman: ok
    Pendiente la integración con cliente

--- webservices/insertar_diseno.php
+++ webservices/insertar_diseno.php
@@ -1,9 +1,23 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+header("Content-Type: text/html; charset=utf-8");
+$method = $_SERVER['REQUEST_METHOD'];
+$JSONData = file_get_contents("php://input");
+$dataObject = json_decode($JSONData);  
+
 	require_once('conectar.php');
 	$conexion=conectarDB();		
-	$datos= array("idUsuario"=>1, "idProductoDiseno"=>6, "descripcion"=>"Prueba 202020" );	
+	
+$idUsuario= $dataObject-> idUsuario;
+$idProductoDiseno= $dataObject-> idProductoDiseno;
+$descripcion= $dataObject-> descripcion;
+	
+	
 	$sentencia= $conexion->prepare("CALL INSERT_DISENO (?,?,?)");
-	$sentencia->bind_param("iis", $datos["idUsuario"], $datos["idProductoDiseno"], $datos["descripcion"] );
+	$sentencia->bind_param("iis", $idUsuario, $idProductoDiseno, $descripcion);
 	
 	$sentencia->execute();
 	$sentencia->close();
